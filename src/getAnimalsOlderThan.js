9d64212db9714fc0ddca0138f94d262af98f0ab8
erikadeolima <erikadeo.lima@hotmail.com>
2022-04-14 16:22:52 -0300

    Requisito 1: concluido

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+function getAnimalsOlderThan(especie, idade) {
+  const minhaEspecie = species.find((animal) => (animal.name === especie)).residents;
+  return minhaEspecie.every((resident) => resident.age > idade);
 }
 
 module.exports = getAnimalsOlderThan;
