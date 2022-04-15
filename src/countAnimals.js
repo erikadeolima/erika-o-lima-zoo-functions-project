afaac07b2e48ae2b0d30df366777a0dffa76211e
erikadeolima <erikadeo.lima@hotmail.com>
2022-04-15 17:02:57 -0300

    Requisito5: concluido

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,6 +2,22 @@ const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  if (animal === undefined) {
+    const especies = data.species.reduce((acc, especie) => {
+      acc[especie.name] = especie.residents.length;
+      return acc;
+    }, {});
+    return especies;
+  }
+  if (Object.keys(animal).length === 1) {
+    const meuAnimal = data.species.find((obj) => obj.name === animal.specie);
+    const quantosAnimais = meuAnimal.residents.length;
+    return quantosAnimais;
+  }
+  if (Object.keys(animal).length === 2) {
+    const meuAnimal2 = data.species.find((especie) => especie.name === animal.specie);
+    return meuAnimal2.residents.filter((sexo) => sexo.sex === animal.sex).length;
+  }
 }
 
 module.exports = countAnimals;
