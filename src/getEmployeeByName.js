4669cf614b1c98c0c24a275b6b64f902f1524199
erikadeolima <erikadeo.lima@hotmail.com>
2022-04-15 10:28:58 -0300

    Corrrigindo ESLint reuqisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,5 +1,5 @@
-const { employees } = require("../data/zoo_data");
-const data = require("../data/zoo_data");
+const { employees } = require('../data/zoo_data');
+const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   if (employeeName === undefined) {
@@ -7,7 +7,7 @@ function getEmployeeByName(employeeName) {
   }
   return employees.find(
     (employee) =>
-      employee.firstName === employeeName || employee.lastName === employeeName
+      employee.firstName === employeeName || employee.lastName === employeeName,
   );
 }
 
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,15 @@
 const data = require('../data/zoo_data');
 
-function isManager(id) {
-  // seu código aqui
+function isManager(cod) {
+  return data.employees.some((gerenciados) => gerenciados.managers.includes(cod));
 }
 
-function getRelatedEmployees(managerId) {
-  // seu código aqui
+function getRelatedEmployees(isManager, managerId) {
+  if (isManager === true) {
+    
+    return haba;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
